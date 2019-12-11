dfe4cef54533f81ba27d4ba8ef980c0204759999
connelly7 <chiannerose@gmail.com>
2019-12-11 17:46:55 -0500

    col layout and text animation in nav panel

--- src/App.js
+++ src/App.js
@@ -1,28 +1,48 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <div>
-        <h1 >Welcome to the rodeo. ye haw</h1>
-      </div>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+       <div className='row'>
+          <div className='black-column'>
+            Some Text in black column
+          </div>
+          <div className='green-column'>
+            <p class="main-paragraph hover_effect">Home</p>
+            <p class="main-paragraph hover_effect">Bio</p>
+            <p class="main-paragraph hover_effect">Resume</p>
+            <p class="main-paragraph hover_effect">Skills</p>
+            <p class="main-paragraph hover_effect">Projects</p>
+            <p class="main-paragraph hover_effect">Contact</p>
+          </div>
+          <div className='content-column'>
+            Some Text in content column
+          </div>
+          <div className='black-column'>
+            Some Text in black column
+          </div>
+        </div>
     </div>
   );
 }
 
 export default App;
+
+
+// <header className="App-header">
+//         {/* <img src={logo} className="App-logo" alt="logo" /> */}
+//         <div id="rectangle"></div>
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
