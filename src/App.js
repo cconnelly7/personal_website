0eec19be597cc21c2b600eea429fe5258247b0a2
connelly7 <chiannerose@gmail.com>
2019-12-11 14:54:21 -0500

    hello world website

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div>
+        <h1 >Welcome to the rodeo. ye haw</h1>
+      </div>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,7 +20,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
