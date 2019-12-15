bcc24aa6ec2137774dc8fd60706df080eb1573a8
connelly7 <chiannerose@gmail.com>
2019-12-15 18:00:51 -0500

    layout adjusts sizes with no flex

--- src/app/Main.js
+++ src/app/Main.js
@@ -12,38 +12,39 @@ class Main extends React.Component {
         this.refB = React.createRef();
         this.refC = React.createRef();
     }
-
     render () {
         return (
             <div className='row'>
-                <div className='black-column'>
-                    Some Text in black column
-                </div>
-                <div className='green-column'>
-                    <button className="main-paragraph hover_effect">Home</button>
-                    <button className="main-paragraph hover_effect" onClick={()=>{
-                        console.log(this.refB)
-                        console.log(this.refB.current.offsetTop)
-                        window.scrollTo(0, this.refB.current.offsetTop);
-                    }}>Bio</button>
-                    <button className="main-paragraph hover_effect"onClick={()=>{
-                        console.log(this.refC)
-                        console.log(this.refC.current.offsetTop)
-                        window.scrollTo(0, this.refC.current.offsetTop);
-                    }}>Resume</button>
-                    <p className="main-paragraph hover_effect">Skills</p>
-                    <p className="main-paragraph hover_effect">Projects</p>
-                    <p className="main-paragraph hover_effect">Contact</p>
-                </div>
-                <div className='content-column'>
+                <div className='white-column'>
+                {/*<div className='green-column-container'> */}
+                    <div className='green-column'>
+                        <button className="main-paragraph hover_effect"onClick={()=>{
+                            console.log(this.refA)
+                            console.log(this.refA.current.offsetTop)
+                            this.refA.current.scrollIntoView({behavior:'smooth'})
+                        }}>Home</button>
+                        <button className="main-paragraph hover_effect" onClick={()=>{
+                            console.log(this.refB)
+                            console.log(this.refB.current.offsetTop)
+                            // window.scroll({top: this.refB.current.offsetTop, behavior: 'smooth'});
+                            this.refB.current.scrollIntoView({behavior:'smooth'})
+                        }}>Bio</button>
+                        <button className="main-paragraph hover_effect"onClick={()=>{
+                            console.log(this.refC)
+                            console.log(this.refC.current.offsetTop)
+                            this.refC.current.scrollIntoView({behavior:'smooth'})
+                        }}>Resume</button>
+                        <p className="main-paragraph hover_effect">Skills</p>
+                        <p className="main-paragraph hover_effect">Projects</p>
+                        <p className="main-paragraph hover_effect">Contact</p>
+                    </div>
+                    <div className='content-column'>
                     <ContentPanel >
                         <PanelA ref={this.refA} />
                         <PanelB ref={this.refB} />
                         <PanelC ref={this.refC} />
                     </ContentPanel>
-                </div>
-                <div className='black-column'>
-                    Some Text in black column
+                    </div>
                 </div>
             </div>
         )
