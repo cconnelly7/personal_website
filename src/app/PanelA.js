aaf85f2d807e430d900bb38e1dd3e3e9a82068bc
connelly7 <chiannerose@gmail.com>
2019-12-16 13:29:59 -0500

    text translate up on home pg

--- src/app/PanelA.js
+++ src/app/PanelA.js
@@ -5,7 +5,10 @@ const PanelA = React.forwardRef((props, ref)=>{
 
     return (
         <div className='column'>
-            <div className='box' ref={ref}></div> 
+            <div className='box' ref={ref}>
+                <h1 class="header fade-up">Hello!</h1>
+                <h1 class="header fade-up">I'm Chianne</h1>
+            </div> 
         </div>
     )
 }) 
