0f6e1094ecb9659680e518397435b5693abd78ca
connelly7 <chiannerose@gmail.com>
2019-12-20 11:01:44 -0700

    some contact stuff

--- src/app/Contact.js
+++ src/app/Contact.js
@@ -5,7 +5,21 @@ const Contact = React.forwardRef((props, ref)=>{
 
     return (
         <div className='column' ref={ref}>
-            <div className='box2' ></div>
+            <div className='box2' height='100vh'>
+                <div className='content'>
+                    <div data-aos='fade-in' className='sectionHeader'>Contact</div>
+                    <div className='whiteSpace'></div>
+                </div>
+
+                <div className='content'>
+                    <div className='squarePhone'></div>
+                    <div className='rectangle'> (480) 467 - 9108</div>
+                </div>
+                <div className='content'>
+                    <div className='squareEmail'></div>
+                    <div className='rectangle'> chiannerose@gmail.com</div>
+                </div>
+            </div>
         </div>
     )
 }) 
