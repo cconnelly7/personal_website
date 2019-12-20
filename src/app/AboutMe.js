4c6736926c6c553fde5c8acbf9ec9bc7b2097498
connelly7 <chiannerose@gmail.com>
2019-12-19 17:36:28 -0700

    random stuff

--- src/app/AboutMe.js
+++ src/app/AboutMe.js
@@ -4,34 +4,32 @@ import './AboutMe.css';
 const AboutMe = React.forwardRef((props, ref)=>{
 
     return (
-        <div className='column'>
-            <div className='box2' ref={ref}>
-                <div className='content'>
-                    <div className='aboutMe'>About Me</div>
-                    <div className='whiteSpace'></div>
-                </div>
-                
-                <div className='content'>
-                    <div className='text'>
-                        <div data-aos='fade-up'>
-                            I am Chianne Connelly. I just graduated from Georgia Tech and began my career as
-                            a Software Engineer. Check out this video to learn more!
-                        </div>
+        <div className='box2' ref={ref}>
+            <div className='content'>
+                <div className='aboutMe'>About Me</div>
+                <div className='whiteSpace'></div>
+            </div>
+            
+            <div className='content'>
+                <div className='text'>
+                    <div data-aos='fade-up'>
+                        I am Chianne Connelly. I just graduated from Georgia Tech and began my career as
+                        a Software Engineer. Check out this video to learn more!
                     </div>
-                    {/* <div data-id="1" data-aos="fade-up"> */}
-                    <div className="wrapper">
-                        <div className="h_iframe">
-                            <img className="ratio" src="http://placehold.it/16x9" />
-                            <iframe src="https://www.youtube.com/embed/Nm_f_44ZgLM" frameBorder="0" allowFullScreen></iframe>
-                        </div>
-                    </div>
-                    {/* </div> */}
-
-
                 </div>
+                {/* <div data-id="1" data-aos="fade-up"> */}
+                <div className="wrapper">
+                    <div className="h_iframe">
+                        <img className="ratio" src="http://placehold.it/16x9" />
+                        <iframe src="https://www.youtube.com/embed/Nm_f_44ZgLM" frameBorder="0" allowFullScreen></iframe>
+                    </div>
+                </div>
+                {/* </div> */}
 
-            </div> 
-        </div>
+
+            </div>
+
+        </div> 
     )
 }) 
 
--- src/app/Project1.js
+++ src/app/Project1.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Project1() {
     return(
       <div data-aos='fade-up'>
-        {/* Helloooo! */}
+        Project 1
       </div>
     )
 }
