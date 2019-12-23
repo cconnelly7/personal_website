d1d2f19b57cdf62aed1e8053d2f3d5a691c84525
connelly7 <chiannerose@gmail.com>
2019-12-22 19:09:59 -0700

    nav btn highlight is cooler/cleaner now

--- src/app/Skills.js
+++ src/app/Skills.js
@@ -2,15 +2,6 @@ import React from 'react';
 import './ContentPanel.css';
 import './Skills.css'
 
-var bgColors = { 
-                    "Orange": "#EA8D4E",
-                    "Red": "#E46765",
-                    "Blue": "#3E99CD",
-                    "Navy": "#4A637B",
-                    "Purple": "#807798",
-                    "Pink": "#D46E8F"
-};
-
 const Skills = React.forwardRef((props, ref)=>{
 
     return (
@@ -24,63 +15,37 @@ const Skills = React.forwardRef((props, ref)=>{
             <ul class="timeline">
                 <li>
                     <div class="direction-r">
-                        <span class="time-wrapper"><span class="time">Dec 2014</span></span>
-                        <div class="flag-wrapper">
+                        <span class="time-wrapper" data-aos="fade-right"><span class="time">Dec 2014</span></span>
+                        <div class="flag-wrapper" >
                             <span class="circ"data-aos="zoom-out"></span>
-                            <span class="flag">React</span>
+                            <span class="flag" >React</span>
                         </div>
-                    <   div class="desc">Lorem ipsum Nisi labore aute do aute culpa magna nulla voluptate exercitation deserunt proident.</div>
+                    <   div class="desc" data-aos="fade-left" >Lorem ipsum Nisi labore aute do aute culpa magna nulla voluptate exercitation deserunt proident. </div>
                     </div>
                 </li>
 
                 <li>
                     <div class="direction-l">
-                        <span class="time-wrapper"><span class="time">Dec 2014</span></span>
+                        <span class="time-wrapper" data-aos="fade-left"><span class="time">Dec 2014</span></span>
                         <div class="flag-wrapper">
                             <span class="circ" data-aos="zoom-out" ></span>
                             <span class="flag">Lorem ipsum Anim.</span>
                         </div>
-                    <div class="desc">Lorem ipsum In ut sit in dolor nisi ex magna eu anim anim tempor dolore aliquip enim cupidatat laborum dolore.</div>
+                    <div class="desc" data-aos="fade-right">Lorem ipsum In ut sit in dolor nisi ex magna eu anim anim tempor dolore aliquip enim cupidatat laborum dolore.</div>
                     </div>
                 </li>
 
                 <li>
                     <div class="direction-r">
-                        <span class="time-wrapper"><span class="time">July 2014</span></span>
+                        <span class="time-wrapper" data-aos="fade-right"><span class="time">July 2014</span></span>
                         <div class="flag-wrapper">
                             <span class="circ" data-aos="zoom-out"></span>
-                            <span class="flag">Lorem Occaecat.</span>
+                            <span class="flag" data-aos="fade-left">Lorem Occaecat.</span>
                         </div>
-                    <div class="desc">Lorem ipsum Minim labore Ut cupidatat quis qui deserunt proident fugiat pariatur cillum cupidatat reprehenderit sit id dolor consectetur ut.</div>
+                    <div class="desc" data-aos="fade-left">Lorem ipsum Minim labore Ut cupidatat quis qui deserunt proident fugiat pariatur cillum cupidatat reprehenderit sit id dolor consectetur ut.</div>
                     </div>
                 </li>
             </ul>
-
-            {/* <div data-aos="zoom-out" data-aos-duration="2000" className='dot'></div>
-            <div data-aos="zoom-out" data-aos-duration="2000" className='dot'></div>
-            <div data-aos="zoom-out" data-aos-duration="2000" className='dot'></div>
-            <div className='row' style={{zIndex: 2}}>
-                <div className='circleIcon'></div>
-                <div class="circle">
-                    <div class="inner"></div>
-                </div>
-                <div data-aos="zoom-out" data-aos-duration="2000" className='dot'></div>
-                <div data-aos="zoom-out" data-aos-duration="2000" className='dot'></div>
-                <div data-aos="zoom-out" data-aos-duration="2000" className='dot'></div>
-                <div className='skill' style={{backgroundColor: bgColors.Orange}} data-aos="zoom-out">React</div>
-                <div data-aos="zoom-out" data-aos-duration="2000" className='dot'></div>
-                <div data-aos="zoom-out" data-aos-duration="2000" className='dot'></div>
-                <div data-aos="zoom-out" data-aos-duration="2000" className='dot'></div>
-                <div class="inner"></div>
-                <div className='skillText'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
-            </div>
-            <div class="circle" style={{zIndex: 2}}>
-                <div class="inner"></div>
-            </div>
-            <div data-aos="zoom-out" data-aos-duration="2000" className='dot'></div>
-            <div data-aos="zoom-out" data-aos-duration="2000" className='dot'></div>
-            <div data-aos="zoom-out" data-aos-duration="2000" className='dot'></div> */}
-
             <div className='moreSpace'></div>
         </div>
     )
