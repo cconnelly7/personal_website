a6566cd51e580e88092f54bc3fef042302a3a70f
connelly7 <chiannerose@gmail.com>
2020-01-07 15:27:44 -0500

    Skills icons

--- src/app/Skills.js
+++ src/app/Skills.js
@@ -15,13 +15,13 @@ const Skills = React.forwardRef((props, ref)=>{
             <ul class="timeline">
                 <li>
                     <div class="direction-r">
-                        <span class="time-wrapper" data-aos="fade-right"><span class="time">Dec 2014</span></span>
                         <div class="flag-wrapper" >
                             <span class="circ"data-aos="zoom-out"></span>
                             <span class="flag" >Web Dev/React</span>
                         </div>
                         <div class="desc" data-aos="fade-left" >From working at NCR and through school at GT, I have gained skills 
                         with React, CSS, HTML, jQuery, and JavaScript. </div>
+                        <img class='logo' data-aos="fade-left" src='./logos/icon1.png'></img>
                     </div>
                 </li>
 
@@ -31,21 +31,22 @@ const Skills = React.forwardRef((props, ref)=>{
                             <span class="circ" data-aos="zoom-out" ></span>
                             <span class="flag">App Dev/React Native</span>
                         </div>
-                        <span class="time-wrapper" data-aos="fade-left"><span class="time">Dec 2014</span></span>
                     <div class="desc" data-aos="fade-right">This past semester I was working on a project in React Native for the Innovation Lab at NCR. 
-                    While there, I built up my skills with JavaScript, redux, their APIs and services used, React Native, and CSS. I also worked on a project in React Native while working with Verizon.</div>
+                    While there, I built up my skills with JavaScript, redux, their APIs and services used, React Native, HTML, and CSS. I also worked on a project in React Native while working with Verizon.</div>
+                    <img class='logo' data-aos="fade-right" src='./logos/icon2.png'></img>
                     </div>
                 </li>
 
                 <li>
                     <div class="direction-r">
-                        <span class="time-wrapper" data-aos="fade-right"><span class="time">July 2014</span></span>
                         <div class="flag-wrapper">
                             <span class="circ" data-aos="zoom-out"></span>
                             <span class="flag" data-aos="fade-left">C#/Unity 3D</span>
                         </div>
                     <div class="desc" data-aos="fade-left"> From developing games with the Unity game engine, I have skills with game AI, state machines, player animations, C#, and game design principles. 
                     </div>
+                    <img class='logo2' data-aos="fade-left" src='./logos/Csharp_Logo.png'></img>
+                    <img class='logo2' data-aos="fade-left" src='./logos/unity.png'></img>
                     </div>
                 </li>
 
@@ -55,9 +56,37 @@ const Skills = React.forwardRef((props, ref)=>{
                             <span class="circ" data-aos="zoom-out" ></span>
                             <span class="flag">Android/Java</span>
                         </div>
-                        <span class="time-wrapper" data-aos="fade-left"><span class="time">Dec 2014</span></span>
                     <div class="desc" data-aos="fade-right">Java was my first computer language I learned indepth and I have worked on projects in Android Studio for
                     hackathons, class projects, and during my time with Verizon. This is where I began gaining skills with Java as an Object Oriented language and with basic application development practices.</div>
+                    <img class='logo3' data-aos="fade-right" src='./logos/android.png'></img>
+                    <img class='logo3' data-aos="fade-right" src='./logos/androidStudio.png'></img>
+                    <img class='logo3' data-aos="fade-right" src='./logos/java.png'></img>
+                    </div>
+                </li>
+                <li>
+                    <div class="direction-r">
+                        <div class="flag-wrapper">
+                            <span class="circ" data-aos="zoom-out"></span>
+                            <span class="flag" data-aos="fade-left">Others</span>
+                        </div>
+                    <div class="desc" data-aos="fade-left"> During my time in school and working both at Verizon and NCR I have become familiar 
+                    with agile practices, SCRUM, JIRA, Bitbucket, Github, Git, and other development tools.
+                    </div>
+                    <img class='logo3' data-aos="fade-left" src='./logos/github.png'></img>
+                    <img class='logo3' data-aos="fade-left" src='./logos/jira.png'></img>
+                    <img class='logo2' data-aos="fade-left" src='./logos/bitbucket.png'></img>
+                    <img class='logo3' data-aos="fade-left" src='./logos/Git.png'></img>
+                    </div>
+                </li>
+                <li>
+                    <div class="direction-l">
+                        <div class="flag-wrapper">
+                            <span class="circ" data-aos="zoom-out" ></span>
+                            <span class="flag">Design</span>
+                        </div>
+                    <div class="desc" data-aos="fade-right">
+                        While studying Industrial Design at Georgia Tech I developed skills with ideation, brainstorming, rapid prototyping, research, and iterative design. 
+                    </div>
                     </div>
                 </li>
             </ul>
